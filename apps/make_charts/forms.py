b11024b4e4dd2e2c572319849dd7bac3cb5e0e55
Katrina Huber <Katrina@Katrina-Hubers-Computer.local>
2019-09-27 12:03:09 -0700

    lots of work on a silly UI detail.

--- apps/make_charts/forms.py
+++ apps/make_charts/forms.py
@@ -4,15 +4,15 @@ from .models import Section, Topic, Tag, Resource, Fail, Question, Practice, Str
 
 
 class SectionForm(forms.Form):
-    section_title = forms.CharField(max_length=45)
+    section_title = forms.CharField(widget=forms.TextInput(attrs={'class' : 'title'}))
     
 
 
 class TopicForm(forms.Form):
-    topic_title = forms.CharField(max_length=45)
+    topic_title = forms.CharField(widget=forms.TextInput(attrs={'class' : 'title'}))
     # section = forms.ModelChoiceField(queryset=Section.objects.all(), empty_label=None)
     # ??? Why can't I get the ModelChoiceField (above) to display the title instead of "Section Object"???
 
 class TagForm(forms.Form):
-    tag_title = forms.CharField(max_length=45)
+    tag_title = forms.CharField(widget=forms.TextInput(attrs={'class' : 'title'}))
     
--- apps/make_charts/views.py
+++ apps/make_charts/views.py
@@ -33,38 +33,57 @@ def add_practice(request):
     return render(request, 'make_charts/practice_questions.html', context)
 
 
-# @login_required(login_url='/login/') #TODO: add back in
-def add_section(request):
+
+def add_section_context():
     sec_form = SectionForm()
     tag_form = TagForm()
-    tag_form = TagForm()
-    # print(tag_form)
     topic_form = TopicForm()
-    if request.method == 'POST':
-        bound_form = SectionForm(request.POST)
-        print(bound_form)
-        if bound_form.is_valid():
-            this_user = User.objects.last() #TODO: make this the user in session!!!!!!!!
-            Section.objects.create(title=request.POST['section_title'], user=this_user)
-        context = {
-            'sections': Section.objects.all(),
+    context = {
+            'sections': Section.objects.all().order_by('-created_at'),
+            'topics': Topic.objects.all(),
+            'tags': Tag.objects.all(),
             'tagForm': tag_form,
             'secForm': sec_form,
             'topicForm': topic_form 
-        }
-        return render(request, 'make_charts/section_topics_tags.html', context)
-    context = { 
-            'sections': Section.objects.all(),
-            'secForm': sec_form,
-            'topicForm': topic_form 
-        }
+    }
+    return context
+    
+
+# @login_required(login_url='/login/') #TODO: add back in
+def add_section(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        this_user = User.objects.last() #TODO: make this the user in session!!!!!!!!
+        new_section = Section.objects.create(title=title, user=this_user)
+        # print(new_section)
+        context = add_section_context()
+        context['section'] = new_section
+        return render(request, 'make_charts/section_selected.html', context)
+    context = add_section_context()
     return render(request, 'make_charts/section_topics_tags.html', context)
+
+
+# # @login_required(login_url='/login/') #TODO: add back in
+# def create_section(request):
+#     if request.method == 'POST':
+#         bound_form = SectionForm(request.POST)
+#         print("bound form *****"*4)
+#         print(bound_form)
+#         if bound_form.is_valid():
+#             print("bound form is vailid *****"*4)
+#             this_user = User.objects.last() #TODO: make this the user in session!!!!!!!!
+#             Section.objects.create(title=request.POST['section_title'], user=this_user)
+#             context = context = add_section_context()
+#         return render(request, 'make_charts/section_topics_tags.html', context)
+#     context = add_section_context()
+#     return render(request, 'make_charts/section_topics_tags.html', context)
 
 # @login_required(login_url='/login/') #TODO: add back in
 def add_topic(request):
-    sec_form = SectionForm()
-    tag_form = TagForm()
-    topic_form = TopicForm()
+    context = add_section_context()
+    print("*********first tag context********"*4)
+    print(context['tags'])
+    print("*****************"*4)
     if request.method == 'POST':
         bound_form = TopicForm(request.POST)
         print(bound_form)
@@ -72,45 +91,20 @@ def add_topic(request):
             this_user = User.objects.last() #TODO: make this the user in session!!!!!!!!
             sec = Section.objects.get(id=request.POST["section"])
             Topic.objects.create(title=request.POST['topic_title'], section=sec)
-        context = {
-            'sections': Section.objects.all(),
-            'tagForm': tag_form,
-            'secForm': sec_form,
-            'topicForm': topic_form 
-        }
-        return render(request, 'make_charts/section_topics_tags.html', context)
-    context = { 
-            'sections': Section.objects.all(),
-            'tagForm': tag_form,
-            'secForm': sec_form,
-            'topicForm': topic_form 
-        }
     return render(request, 'make_charts/section_topics_tags.html', context)
 
 # @login_required(login_url='/login/') #TODO: add back in
 def add_tag(request):
-    sec_form = SectionForm()
-    tag_form = TagForm()
-    topic_form = TopicForm()
+    context = add_section_context()
     if request.method == 'POST':
-        bound_form = TopicForm(request.POST)
-        print(bound_form)
-        if bound_form.is_valid():
-            Tag.objects.create(title=request.POST['tag_title'])
-
-        context = {
-            'sections': Section.objects.all(),
-            'tagForm': tag_form,
-            'secForm': sec_form,
-            'topicForm': topic_form 
-        }
+        print(request.POST)  #<QueryDict: 
+                                        #{'csrfmiddlewaretoken': ['TQPBV2iCXdAvpfR0dzAJnz3egRL1Ok1QERXYX3Qc9JxGNuqeCeHfoZW6Wak0Mtex'], 
+                                        #'title': ['Tag title here']}>
+        easy_dict = request.body
+        print("*"*40)
+        print(easy_dict)
         return render(request, 'make_charts/section_topics_tags.html', context)
-    context = { 
-            'sections': Section.objects.all(),
-            'tagForm': tag_form,
-            'secForm': sec_form,
-            'topicForm': topic_form 
-        }
+    
     return render(request, 'make_charts/section_topics_tags.html', context)
 
 
